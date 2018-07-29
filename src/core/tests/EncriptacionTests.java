b2062630a4d3ccc79c3eb956916e9f1fdd7dec8a
JLeo7 <jleomoram@gmail.com>
2018-07-29 00:49:34 -0600

    excepciones de tests y mensaje mas preciso

--- src/core/tests/EncriptacionTests.java
+++ src/core/tests/EncriptacionTests.java
@@ -15,25 +15,25 @@ public class EncriptacionTests {
     static BlowfishManager blowfish;
 
     @BeforeClass
-    static public void init() throws Exception {
+    static public void init() {
         aes = new SymetricManager();
         rsa = new AsymetricManager();
         blowfish = new BlowfishManager();
     }
 
-    @Test
+    @Test (expected = Exception.class)
     public void TestEntriptarAES() throws Exception {
         assertEquals("jJ37lG7Jr1oQBpQWBUT8PZPhBPO6DLGz8IkbbN7LGx4=", aes.encryptMessage("mensajito", "aqui va el mensajito", "llaveaes"));
     }
 
-    // La generacion de la llave con el metodo RSA genera un mensaje encriptado cada vez, aun con la misma llave y con el mismo nombre y contenido de mensaje. Por eso no se incluye la prueba de encriptacion con RSA.
+    // Encriptar un mensaje con el mentodo RSA genera un mensaje encriptado diferente cada vez, aun con la misma llave y con el mismo nombre y contenido de mensaje. Por eso no se incluye la prueba de encriptacion con RSA.
 
-    @Test
+    @Test (expected = Exception.class)
     public void TestDesentriptarAES() throws Exception {
         assertEquals("aqui va el mensajito", aes.decryptMessage("mensajito", "llaveaes"));
     }
 
-    @Test
+    @Test (expected = Exception.class)
     public void TestDesentriptarRSA() throws Exception {
         assertEquals("aqui va el mensajito", rsa.decryptMessage("mensajito", "llaversa"));
     }
@@ -43,7 +43,7 @@ public class EncriptacionTests {
         assertEquals("uUI1HlxGQQUkjx1H356HUiAiXc4EKVOt", blowfish.encryptMessage("mensajito", "aqui va el mensajito", "llavebf"));
     }
 
-    @Test
+    @Test (expected = Exception.class)
     public void TestDesentriptarBlowFish() throws Exception {
         assertEquals("aqui va el mensajito", blowfish.decryptMessage("mensajito", "llavebf"));
     }
